57cae81757d7957b89f28c01b3c09ee3897b11bb
mosheyah <dmosheyah@gmail.com>
2023-08-30 02:41:18 +0200

    ALGO DIVISION ET SOMME

--- jupyter.py
+++ jupyter.py
@@ -3,4 +3,14 @@ b = int(input("entrer la valeur de b : "))
 c = a+b
 print("La somme de deux nombre reels ",a,"+",b,"=",c)
 
+# Algorithme division
+
+C = float(input("Entrer le nombre a  : "))
+E = float(input("Enter la valeur de b : "))
+if E!=0 :
+    q = a/b
+    print(C,"/",E,"=",q)
+else :
+    print("Division impossible ", C,"/",E, "=","{}")
+
 
